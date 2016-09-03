83bf47c257f19c5ffaf70955a6efae7568f0133b
konstantinsmirnov <k.smi@mail.ru>
2016-09-03 16:17:48 +0300

    Remove static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#index'
+  root 'articles#index'
 
   get 'articles/index', as: 'articles'
   get 'about/show', as: 'about'
