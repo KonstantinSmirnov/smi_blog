69ad62327a39ad2d9bda4cf38f108f298aa43586
konstantinsmirnov <k.smi@mail.ru>
2016-09-03 23:35:17 +0300

    Fix delete button

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -36,7 +36,8 @@ class Admin::ArticlesController < Admin::DashboardController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to admin_articles_path, notice: "Article was deleted"
+    flash[:success] = "Article was deleted"
+    redirect_to admin_articles_path
   end
 
   private
