235c88d700969ca0458aec7a6830f1bf062894c0
Konstantin <rusksmi@gmail.com>
2016-09-06 06:42:22 +0000

    Add precompile assets task for Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -91,10 +91,37 @@ namespace :deploy do
     end
   end
 
+  namespace :assets do
+    Rake::Task['deploy:assets:precompile'].clear_actions
+
+    desc "Precompile assets on local machine and upload them to the server."
+    task :precompile do
+      run_locally do
+        execute 'RAILS_ENV=production bundle exec rake assets:precompile'
+      end
+
+      on roles(:web) do
+        within release_path do
+          asset_full_path = "#{release_path}/public/#{fetch(:assets_prefix)}"
+          asset_parent_path = File.dirname(asset_full_path)
+          execute "mkdir -p #{asset_full_path}"
+          upload! "./public/#{fetch(:assets_prefix)}", asset_parent_path, recursive: true
+        end
+      end
+
+      run_locally do
+        execute "rm -r ./public/#{fetch(:assets_prefix)}"
+      end
+    end
+  end
+
+
+
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-  after  :finishing,    :restart
+  #after  :finishing,    :restart
+  
 end
 
 # ps aux | grep puma    # Get puma pid
