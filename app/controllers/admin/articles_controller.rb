3b8c5dd7165acbf36ee8e06f6df49076940c5773
konstantinsmirnov <k.smi@mail.ru>
2016-09-08 21:14:55 +0300

    Add selected values for filters

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,24 +1,24 @@
 class Admin::ArticlesController < Admin::DashboardController
   def index
-
     filter = Hash.new
 
     if params[:commit] == 'Filter' && (params[:filter_category] != '' || params[:filter_status] != '')
       if !params[:filter_category].blank?
-        filter.store(:category_fields, {:name => params[:filter_category]})
+        #filter.store(:category_fields, {:name => params[:filter_category]})
+        filter.store(:category_fields, {:name => Category.find(params[:filter_category]).name})
       end
       if !params[:filter_status].blank?
         filter.store(:status, params[:filter_status])
       end
       puts filter
-      @articles = Article.where(filter).order('created_at DESC')  
+      @articles = Article.where(filter).order('created_at DESC')
     else
       @articles = Article.all.reverse
     end
 
-    
-    
-    
+
+
+
   end
 
   def show
@@ -49,9 +49,9 @@ class Admin::ArticlesController < Admin::DashboardController
     @article = Article.find(params[:id])
 
     if @article.update_attributes(article_params)
-      
+
       update_date_of_publication(@article)
-      
+
       flash.now[:success] = "Changes were saved"
       render 'edit'
     else
@@ -71,7 +71,7 @@ class Admin::ArticlesController < Admin::DashboardController
   def article_params
     params.require(:article).permit(:status, :title, :description, :content, :category_id)
   end
-  
+
   def update_date_of_publication(article)
     if article.published?
       article.published_on = DateTime.now if article.published_on == nil
