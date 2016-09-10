bd18b2c54d58ce89cf1150088cfff2dddf32ca53
konstantinsmirnov <k.smi@mail.ru>
2016-09-10 10:28:51 +0300

    Remove home additional root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  get 'home/index', as: 'home'
   get 'articles/index', as: 'articles'
   get 'articles/show', as: 'article'
   get 'categories/show', as: 'category'
