5c4365864d3887903f3cb775f2c21d4e922ed260
konstantinsmirnov <k.smi@mail.ru>
2016-09-10 10:26:07 +0300

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'articles#index'
+  root 'home#index'
 
+  get 'home/index', as: 'home'
   get 'articles/index', as: 'articles'
   get 'articles/show', as: 'article'
   get 'categories/show', as: 'category'
