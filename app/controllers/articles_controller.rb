49d87bb6bbe9b7b423a58405794c3ce06a214412
Konstantin <rusksmi@gmail.com>
2016-09-13 15:02:42 +0000

    Add simple antispam delay filter for comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,7 @@ class ArticlesController < ApplicationController
     @comments = @article.comments.order(created_at: :desc).paginate(:page => params[:page], :per_page => 10 )
     @article.update_attribute("views", @article.views + 1)
     @new_comment = @article.comments.build
+    cookies[:opened_article_id] = params[:id]
+    cookies[:article_opened_at] = Time.now.to_i
   end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,21 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id]) 
     @new_comment = @article.comments.new(comment_params)
-    respond_to do |format|
-      if @new_comment.save
-        flash.now[:success] = "Comment have been added"
-        format.js { render 'create_success.js.erb' }
-      else
-        flash.now[:danger] = "Comment was not added"
-        format.js { render 'create_fail.js.erb' }
+    puts "!!!!! Cookies!"
+    puts cookies[:opened_article_id]
+    puts cookies[:article_opened_at]
+    puts Time.now.to_i - cookies[:article_opened_at].to_i
+    if cookies[:opened_article_id] == params[:article_id] && ( Time.now.to_i - cookies[:article_opened_at].to_i) > 15
+      respond_to do |format|
+        if @new_comment.save
+          format.js { render 'create_success.js.erb' }
+        else
+          format.js { render 'create_fail.js.erb', :locals => { :text => "Comment was not created. Please check if all fields filled correct." } }
+        end
+      end
+    else
+      respond_to do |format|
+        format.js { render 'create_fail.js.erb', :locals => { :text => "Antispam filter requires to wait #{15 - Time.now.to_i + cookies[:article_opened_at].to_i} more seconds after Article is opened." } }
       end
     end
   end
