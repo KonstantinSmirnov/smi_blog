8ae5495a7ee3ff40b711419236c2aac238d0a24c
konstantinsmirnov <k.smi@mail.ru>
2016-09-14 12:47:44 +0300

    Remove comments title

--- app/models/article.rb
+++ app/models/article.rb
@@ -18,7 +18,7 @@ class Article
   belongs_to :category
   alize :category, :name
 
-  has_many :comments
-  has_many :images
+  has_many :comments, dependent: :destroy
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, :allow_destroy => true
 end
