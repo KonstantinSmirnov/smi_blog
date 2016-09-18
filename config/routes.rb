e133d12c58600eece72371754b1e1209d6f0bbd5
konstantinsmirnov <k.smi@mail.ru>
2016-09-18 16:39:32 +0300

    Add search controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home_page#show'
 
-  get 'categories/show', as: 'category'
-  get 'about_page/show', as: 'about_page'
+  get 'categories/show',  as: 'category'
+  get 'about_page/show',  as: 'about_page'
+
+  get 'search/index',     as: 'search'
+
   resources :articles, only: [:index, :show] do
     resources :comments, only: [:create]
   end
