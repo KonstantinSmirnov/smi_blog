dfc518e48dbd31bf9b688d3ca38d9df708ff8783
konstantinsmirnov <k.smi@mail.ru>
2016-09-20 00:09:59 +0300

    Replace time to prevent coment creating and check if sommon settings exist

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    @article = Article.find_by(:slug => params[:article_id]) 
+    @article = Article.find_by(:slug => params[:article_id])
     @new_comment = @article.comments.new(comment_params)
     puts "!!!!! Cookies!"
     puts cookies[:opened_article_id]
     puts cookies[:article_opened_at]
     puts Time.now.to_i - cookies[:article_opened_at].to_i
-    if cookies[:opened_article_id] == params[:article_id] && ( Time.now.to_i - cookies[:article_opened_at].to_i) > 15
+    if cookies[:opened_article_id] == params[:article_id] && ( Time.now.to_i - cookies[:article_opened_at].to_i) > 5
       respond_to do |format|
         if @new_comment.save
           format.js { render 'create_success.js.erb' }
@@ -16,14 +16,14 @@ class CommentsController < ApplicationController
       end
     else
       respond_to do |format|
-        format.js { render 'create_fail.js.erb', :locals => { :text => "Antispam filter requires to wait #{15 - Time.now.to_i + cookies[:article_opened_at].to_i} more seconds after Article is opened." } }
+        format.js { render 'create_fail.js.erb', :locals => { :text => "Antispam filter requires to wait #{5 - Time.now.to_i + cookies[:article_opened_at].to_i} more seconds after Article is opened." } }
       end
     end
   end
-  
-  
+
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:author_name, :text)
   end
