1baed504d761387569564b3dffe44c4378e3f2c2
konstantinsmirnov <k.smi@mail.ru>
2016-09-21 00:56:23 +0300

    Add common layout for blog

--- app/controllers/blog/search_controller.rb
+++ app/controllers/blog/search_controller.rb
@@ -1,5 +1,6 @@
 class Blog::SearchController < BlogController
   def index
+    @popular_articles = Article.published.order(views: :desc).limit(5)
     if params[:search_query].lstrip.chop != ''
       @found_articles = Article.published.full_text_search(params[:search_query]).paginate(:page => params[:page], :per_page => 20 )
     end
