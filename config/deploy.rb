54d72dd3381be90970c11c9e04e399c4d174e216
konstantinsmirnov <k.smi@mail.ru>
2016-09-22 23:44:45 +0300

    Add rm task to clean public/system folder when cap reset

--- config/deploy.rb
+++ config/deploy.rb
@@ -105,6 +105,7 @@ namespace :deploy do
       within release_path do
         with rails_env: fetch(:rails_env) do
           execute :rake, "db:reset"
+          execute "rm -rf #{shared_path}/public/system/*"
         end
       end
     end
