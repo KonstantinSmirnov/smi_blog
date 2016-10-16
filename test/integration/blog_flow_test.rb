3e167e7abfb2f2f1d00e2c0018d3e398555841ff
konstantinsmirnov <k.smi@mail.ru>
2016-10-16 11:05:29 +0300

    Add integration test

---
+++ test/integration/blog_flow_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class BlogFlowTest < ActionDispatch::IntegrationTest
+  test "should have Blog tab" do
+    get "/"
+    assert_select "li", "Blog", "Has not Blog tab"
+  end
+
+  # test "can create an article" do
+  #   get "/articles/new"
+  #   assert_response :success
+  #
+  #   post "/articles",
+  #     params: { article: { title: "can create", body: "article successfully." } }
+  #   assert_response :redirect
+  #   follow_redirect!
+  #   assert_response :success
+  #   assert_select "p", "Title:\n  can create"
+  # end
+
+
+end
